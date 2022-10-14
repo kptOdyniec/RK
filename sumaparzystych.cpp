b82097e2a3f8baff28f608ab99335e87b79c0436
kptOdyniec <r.kolaska@gmail.com>
2022-10-14 15:22:27 +0200

    Add files via upload

---
+++ sumaparzystych.cpp
@@ -0,0 +1,27 @@
+﻿//Podstawy programowania zadnaie domowe 1.3
+//Program sumujacy liczby parzyste
+//Rafał Kolaska nr albumu 410217
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+    int a, b;
+    cout << "Dla zakresu liczbowego postaci <a, b> " << endl;
+    cout << "Podaj liczby okreslające zakres: \n";
+    cout << "\na = ";
+    cin >> a;
+    cout << "\nb = ";
+    cin >> b;
+    int sum = 0;
+    for (int i = a; i <= b; i++) {
+        if (i % 2 == 0) {
+            sum = sum + i;
+        }
+    }
+    cout << "\nSuma liczb parzystych z tego przedzialu równa sie: " << sum;
+    return 0;
+}
+
